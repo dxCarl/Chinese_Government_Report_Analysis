acd51389c65a978df2c50652a5f4b9672e70618c
745125420 <745125420@qq.com>
2021-02-18 23:28:21 +0800

    python2 -> python3

--- filter_ReportLinks.py
+++ filter_ReportLinks.py
@@ -4,21 +4,22 @@ Created on Wed Dec 07 14:29:37 2016
 
 @author: libin
 """
-import urllib2
+import urllib
 import requests
 import re
 import jieba
 import csv
 from bs4 import BeautifulSoup
-import operator 
+import operator
 from collections import defaultdict
 import jieba.analyse
 
+
 def filterReportLinks(url):
-    print '过滤1954-2016年政府工作报告网址······'
-    html_doc = urllib2.urlopen(url).read()
+    print('过滤1954-2016年政府工作报告网址······')
+    html_doc = urllib.urlopen(url).read()
     standard_html = BeautifulSoup(html_doc, "html.parser")
-    for link in standard_html.find_all('a', href = re.compile(r'/content_')):
+    for link in standard_html.find_all('a', href=re.compile(r'/content_')):
         url = link.get('href')
         year = link.get_text()
         Report_Links[year] = url
